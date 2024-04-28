bd0a2971d8899939ee1220ea6e5d23e255237223
Yevhenii Sarancha <yevhenii.sarancha@outlook.com>
2024-04-29 02:47:33 +0300

    minor changes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,11 @@
 import css from "./Profile.module.css";
 
-const UserCard = ({ name, tag, location, avatar, stats: { followers, views, likes } }) => (
+const UserCard = ({ username, tag, location, avatar, stats: { followers, views, likes } }) => (
   <>
     <div className={css.userCard}>
       <div className={css.userCardBody}>
-        <img src={avatar} alt={name} />
-        <p className={css.userName}>{name}</p>
+        <img src={avatar} alt={username} />
+        <p className={css.userName}>{username}</p>
         <p className={css.userTag}>@{tag}</p>
         <p className={css.userLocation}>{location}</p>
       </div>
